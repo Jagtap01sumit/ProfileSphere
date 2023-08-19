8879ba1540336f85633c2e631d5ee7ea428809a2
Sumit <jagtasumit668@gmail.com>
2023-08-19 18:14:54 +0530

    middleware

---
+++ src/middleware.ts
@@ -0,0 +1,29 @@
+import { NextResponse } from 'next/server'
+import { NextRequest } from 'next/server'
+
+
+export function middleware(request: NextRequest) {
+    const path = request.nextUrl.pathname //get a path (name)
+
+    const isPublicpath = path === '/login' || path === '/signup' //if having a path name== '/login' or any other normal path then its public path 
+
+    const token = request.cookies.get('token')?.value || '' //user have token or not
+
+    if (isPublicpath && token) { //if have a public path and token then u can access profile page
+        return NextResponse.redirect(new URL('/profile', request.nextUrl))
+    }
+    if (!isPublicpath && !token) {
+        return NextResponse.redirect(new URL('/', request.nextUrl));
+    }
+
+
+}
+
+//matching paths
+export const config = {
+    matcher: [
+
+        '/profile',
+        '/login',
+        '/signup'],
+}
